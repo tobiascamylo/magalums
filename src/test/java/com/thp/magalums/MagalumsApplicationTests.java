41726eb6fd24a955e991f41a1ebb967ed54dc4b3
Tobias <tobiascamylo@gmail.com>
2024-07-13 21:23:19 -0300

    initialize project

---
+++ src/test/java/com/thp/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thp.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
